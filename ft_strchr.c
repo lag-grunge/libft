096c521a6ae9801b8d747d4fb91824020301364e
Stevenson Dalton <sdalton@vo-e3.msk.21-school.ru>
2021-04-27 12:30:22 +0300

    270421

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: sdalton <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 10:47:38 by sdalton           #+#    #+#             */
-/*   Updated: 2021/04/27 12:21:13 by sdalton          ###   ########.fr       */
+/*   Updated: 2021/04/27 12:30:15 by sdalton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	*ft_strchr(const char *str, int c)
 	i = 0;
 	cu = c;
 	if (!(*str) && !c)
-		return ((char *)str); 
+		return ((char *)str);
 	while (str[i])
 	{
 		if (cu == (unsigned char)(str[i]))
