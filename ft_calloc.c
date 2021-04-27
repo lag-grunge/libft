2fb84ee3de4dcf549ad15cbf7b7342da34d3901f
Stevenson Dalton <sdalton@vo-e3.msk.21-school.ru>
2021-04-27 11:44:01 +0300

    270421

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,38 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdalton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/27 11:25:05 by sdalton           #+#    #+#             */
+/*   Updated: 2021/04/27 11:27:16 by sdalton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-/** The calloc() function allocates memory for an array of nmemb elements of size bytes each 
- * and returns a pointer to the allocated memory. The memory is set to zero. 
- * If nmemb or size is 0, then calloc() returns either NULL, or a unique pointer value 
- * that can later be successfully passed to free().
- **/
+/** The calloc() function allocates memory for an array of nmemb elements 
+* 	of size bytes each 
+* and returns a pointer to the allocated memory. The memory is set to zero. 
+* If nmemb or size is 0, then calloc() returns either NULL, or a unique 
+* pointer value 
+* that can later be successfully passed to free().
+**/
 
-void    *ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-    size_t bytes;
-    size_t i;
-    void    *memory;
+	size_t	bytes;
+	size_t	i;
+	void	*memory;
 
-    bytes = nmemb * size;
-    if (bytes)
-    {
-        memory = malloc(bytes);
-        if (memory)
-        {
-            while (i < bytes)
-            {
-                ((char *)memory)[i] = 0;
-                i++;
-            }
-        }
-        return (memory);        
-    }
-    return (NULL);
-}
-
-#include <stdio.h>
-
-int main()
-{
-
-    
+	bytes = nmemb * size;
+	if (bytes)
+	{
+		memory = malloc(bytes);
+		i = 0;
+		if (memory)
+		{
+			ft_bzero(memory, bytes);
+			return (memory);
+		}
+	}
+	return (NULL);
 }
