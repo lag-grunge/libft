9d7a2ca821216e46a7023bf808fc2d712b49e6c5
Stevenson Dalton <sdalton@vo-e3.msk.21-school.ru>
2021-04-27 15:41:23 +0300

    270421

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: sdalton <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/26 14:19:37 by sdalton           #+#    #+#             */
-/*   Updated: 2021/04/27 15:24:46 by sdalton          ###   ########.fr       */
+/*   Updated: 2021/04/27 15:41:14 by sdalton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,12 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
 	cp = dst + dst_len;
-	if (size - 1 > dst_len + src_len)
+	if ((int)(size - 1) > (int)(dst_len + src_len))
 	{
 		min = src_len;
 		dst[dst_len + min] = 0;
 	}
-	else if (size - 1 > dst_len)
+	else if ((int)(size - 1) > (int)(dst_len))
 	{
 		min = size - dst_len - 1;
 		dst[dst_len + min] = 0;
@@ -45,7 +45,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	else
 		min = 0;
 	ft_memcpy(cp, src, min);
-	return (dst_len + src_len);
+	return (size + src_len);
 }
 /*
 #include <stdio.h>
@@ -57,8 +57,8 @@ int main()
 	char buff1[0xF00] = "there is no stars in the sky";
 	char buff2[0xF00] = "there is no stars in the sky";
 	size_t max = strlen("the cake is a lie !\0I'm hidden lol\r\n") + 4;
-	size_t r1 = strlcat(buff1, str, max);
-	size_t r2 = ft_strlcat(buff2, str, max);
+	size_t r1 = strlcat(buff1, str, 0);
+	size_t r2 = ft_strlcat(buff2, str, 0);
 	printf("%s\n", buff1);
 	printf("%s\n", buff2);
 }
