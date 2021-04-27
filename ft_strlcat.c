00383c505d867f05417ca6dc228817c143910326
Stevenson Dalton <sdalton@vo-e3.msk.21-school.ru>
2021-04-27 15:21:10 +0300

    270421

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: sdalton <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/26 14:19:37 by sdalton           #+#    #+#             */
-/*   Updated: 2021/04/26 14:50:36 by sdalton          ###   ########.fr       */
+/*   Updated: 2021/04/27 15:01:57 by sdalton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,39 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	char		*cp;
 	size_t		dst_len;
 	size_t		src_len;
-	int			i;
 	size_t		min;
 
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
 	cp = dst + dst_len;
-	i = size - dst_len - 1;
-	if (i > 0)
+	if (size - 1 > dst_len + src_len)
 	{
-		if ((size_t)i < src_len)
-			min = i;
-		else
-			min = src_len;
-		ft_memcpy(cp, src, min);
-		if ((size_t)i > src_len)
-			ft_bzero(cp + min, (size_t)(i - src_len));
-		dst[size - 1] = 0;
+		min = src_len;
+		dst[dst_len + min] = 0;
 	}
+	else if (size - 1 > dst_len)
+	{
+		min = size - dst_len - 1;
+		dst[dst_len + min] = 0;
+	}
+	else
+		min = 0;
+	ft_memcpy(cp, src, min);
 	return (dst_len + src_len);
 }
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char *str = "the cake is a lie !\0I'm hidden lol\r\n";
+	char buff1[0xF00] = "there is no stars in the sky";
+	char buff2[0xF00] = "there is no stars in the sky";
+	size_t max = strlen("the cake is a lie !\0I'm hidden lol\r\n") + 4;
+	size_t r1 = strlcat(buff1, str, max);
+	size_t r2 = ft_strlcat(buff2, str, max);
+	printf("%s\n", buff1);
+	printf("%s\n", buff2);
+}
+*/
