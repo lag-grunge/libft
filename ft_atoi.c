4fe3bf24b0c12fe2e2272a7716456ff40af557aa
Stevenson Dalton <sdalton@vo-e3.msk.21-school.ru>
2021-04-28 11:26:25 +0300

    280421

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: sdalton <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/26 12:54:16 by sdalton           #+#    #+#             */
-/*   Updated: 2021/04/26 19:54:21 by sdalton          ###   ########.fr       */
+/*   Updated: 2021/04/28 11:18:42 by sdalton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,35 @@ static int	ft_isspace(int c)
 	return ((9 <= cu && cu <= 13) || cu == 32);
 }
 
+static int	check_overflow(int n1, int n2, int sign)
+{
+	if (sign == 1)
+	{
+		if (n1 > 2147483647 - n2)
+			return (-1);
+		else
+			return (n1 + n2);
+	}
+	else if (sign == -1)
+	{
+		if (-n1 < -2147483648 + n2)
+			return (-1);
+		else
+			return (-n1 - n2);
+	}
+	else
+		return (0);
+}
+
 int	ft_atoi(char *s)
 {
-	int	n;
-	int	sign;
+	int		n;
+	int		sign;
+	size_t	digits;
 
 	n = 0;
 	sign = 1;
+	digits = 0;
 	while (ft_isspace(*s))
 		s++;
 	if (*s == '-')
@@ -50,7 +72,31 @@ int	ft_atoi(char *s)
 	}
 	else if (*s == '+')
 		s++;
-	while (ft_isdigit(*s))
+	while (ft_isdigit(*s) && digits++ < 10)
 		n = 10 * n + (*s++) - 48;
-	return (n * sign);
+	if (ft_isdigit(*s) && !(ft_isdigit(s[1])))
+		return (check_overflow(9 * n + (*s++) - 48, n, sign));
+	else if (ft_isdigit(*s))
+		return (-1);
+	else
+		return (sign * n);
 }
+/*
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int	i;
+
+	if (argc > 1)
+	{
+		i = 1;
+		while (i < argc)
+		{
+			printf("%d\n", atoi(argv[i]));
+			printf("%d\n", ft_atoi(argv[i]));
+			i++;
+		}
+	}
+
+}*/
