02d464cfa0cd45c13e76bcad1ae372ec5294447d
Stevenson Dalton <sdalton@vo-e3.msk.21-school.ru>
2021-04-30 16:10:57 +0300

    300421

---
+++ ft_lstadd_back.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdalton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/30 14:33:47 by sdalton           #+#    #+#             */
+/*   Updated: 2021/04/30 14:39:19 by sdalton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*	Function name ft_lstadd_back									*/
+/*	Prototype void ft_lstadd_back(t_list **lst, t_list *new);		*/
+/*	Turn in files -                                                 */
+/*	Parameters #1. The address of a pointer to the first link of    */
+/*	a list.                                                         */
+/*	 #2. The address of a pointer to the element to be              */
+/*	added to the list.                                              */
+/*	Return value None                                               */
+/*	External functs. None                                           */
+/*	Description Adds the element ’new’ at the end of the list.      */
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+	
+	last = ft_lstlast(*lst);
+	last->next = new;
+}
--- libft.h
+++ libft.h
@@ -25,6 +25,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(const char *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
@@ -36,5 +37,19 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+
+typedef struct	s_list
+{
+		void *content;
+		struct s_list *next;
+} 				t_list;
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void 	ft_lstadd_back(t_list **lst, t_list *new);
+void 	ft_lstdelone(t_list *lst, void (*del)(void *)); 
+void 	ft_lstclear(t_list **lst, void (*del)(void*));
 
 #endif
