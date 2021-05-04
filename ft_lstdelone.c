375fee1e70fdad3ddca11446c30dee20eec461c4
Stevenson Dalton <sdalton@vo-e3.msk.21-school.ru>
2021-05-04 11:42:53 +0300

    0405

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,13 +6,13 @@
 /*   By: sdalton <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 14:43:08 by sdalton           #+#    #+#             */
-/*   Updated: 2021/04/30 16:10:29 by sdalton          ###   ########.fr       */
+/*   Updated: 2021/05/04 11:42:35 by sdalton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*		Function name ft_lstdelone										*/ 
+/*		Function name ft_lstdelone										*/
 /*		Prototype void ft_lstdelone(t_list *lst, void (*del)(void       */
 /*		*));                                                            */
 /*		Turn in files -                                                 */
@@ -26,7 +26,7 @@
 /*		’de/l’ given as a parameter and free the element.				*/
 /*		Th/e memory of ’next’ must not be freed.						*/
 
-void ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	del(lst->content);
 	free(lst);
