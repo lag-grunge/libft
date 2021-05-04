95d628b04d253a916af3588b8c253955d7012111
Stevenson Dalton <sdalton@vo-e3.msk.21-school.ru>
2021-05-04 13:49:00 +0300

    0405

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: sdalton <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 14:33:47 by sdalton           #+#    #+#             */
-/*   Updated: 2021/05/04 13:27:12 by sdalton          ###   ########.fr       */
+/*   Updated: 2021/05/04 13:48:52 by sdalton          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,28 +32,4 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		*lst = new;
 	else
 		last->next = new;
-}
-
-#include <string.h>
-#include <stdio.h>
-
-int main ()
-{
- 
-	t_list *l = ((void *)0);
- 	t_list *n = ft_lstnew(strdup("OK"));
-                                          
- 	ft_lstadd_back(&l, n);
- 	if (l == n && !strcmp(l->content, "OK"))
- 	{
- 		free(l->next);
-		free(l);
-		printf("TEST_SUCCESS");
-	 }
-	 else
-	 {
-		free(l->next);
-		free(l);
-		printf("TEST_FAILED");
-	 }
 }
